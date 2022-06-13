9764847b0c965b8c47283e6fefeb19428207cad2
hyerimYeom <hyerim.yeom@kakao.com>
2022-06-14 03:13:16 +0900

    #2 Fetching Data
    * Used API from themoviedb.org
    * NEED to recap
      - ? sign at the end of object
      - IIFE
      - fetch() and async&await

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,37 @@
-import Seo from '../components/Head';
-import NavBar from '../components/NavBar';
+import { useEffect, useState } from "react";
+import Seo from "../components/Head";
 
-export default function Home(){
-    return (
-        <>
-        <Seo title="HOME"/>
-        <div>
-            HOME
-        </div>
-        </>
-    );
+const API_KEY = "63f57c7278065e255b2fcfd0ab4eeb68";
+
+export default function Home() {
+  const [movies, setMovies] = useState([]);
+  // useEffect(() =>{
+  //     fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`)
+  // }, [])
+
+  useEffect(() => {
+    (async () => {
+      const { results } = await (
+        await fetch(
+          `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=en-US&page=1`
+        )
+      ).json();
+      //  const json = await response.json();
+      setMovies(results);
+    })();
+  }, []);
+
+  return (
+    <div className="container">
+      <Seo title="HOME" />
+      <div>
+        {!movies && <h4>Loading...</h4>}
+        {movies?.map(movie => (
+          <div key={movie.id}>
+            <h4>{movie.title}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
